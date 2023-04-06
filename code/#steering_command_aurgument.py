ba9ed0ca2e9f4a023af43cd07b9317b6c2f94b86
Natethegreat505 <125489092+Natethegreat505@users.noreply.github.com>
2023-04-06 07:38:35 -0600

    Create #steering_command_aurgument.py

---
+++ code/#steering_command_aurgument.py
@@ -0,0 +1,7 @@
+from jetracer.nvidia_racecar import nvidiaracecar
+import argparse
+car = nvidia_racecar()
+parser = argparse.ArgumentParser()
+parser.add_argument('--value', type+float, required=True)
+args = parser.parse_args()
+car.steering = args.value
